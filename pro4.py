8ab80102743d439f62b58ab4e3e4cd77bbe28f1f
Gowtham12eu <gk9573870@gmail.com>
2025-05-22 21:42:50 +0530

    Add files via upload

---
+++ pro4.py
@@ -0,0 +1,18 @@
+var1=input("I need one help  in direction? ")
+var1=input ("where is taj mahal ? Do you have know the path of the taj mahal: ")
+if var1=="Yes":
+    print("You have to go straight to far 10km. ")
+else:
+    print("sorry i do know the place")
+x=input("there is two path to go to the taj mahal and the  one path is bridge and another path is forest.Where you like to go forest/bridge to contiue the trip?")
+if x=="forest":
+    print("You want to take left to go to the taj mahal on while driving you see the forest and lot of animal and natures.It is 3km to reach taj mahal")
+elif x=="Bridge":
+    print("when to take right there is the straight bridge to go to the taj mahal.the sceneari is lookin good in the bridge while you are driving in the bridge")
+else:
+    print("Go straight the path of the road is difficult to driving the vechiles :( ")
+d=("Thankyou for the information mister (: and what is your name ? ")
+print(d)
+e=input("My name is :")
+d=("ok tata " + e)
+print(d)
